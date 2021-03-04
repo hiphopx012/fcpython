abcce21dab263f1074bc1f560eee2bd39c84ce57
hiphopx012 <79928701+hiphopx012@users.noreply.github.com>
2021-03-04 21:28:20 +0800

    Add files via upload

---
+++ EX0304.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#elif 是else if的意思，可以連續判斷
+weight=50
+
+if weight>=100:
+    print("要減肥了")
+elif weight>=80:
+    print("多運動") 
+elif weight>=60:
+    print("多吃點") 
+    
+else:
+    print("太瘦了")
+    print("程式執行完畢")
